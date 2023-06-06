1f6b6384328efcc8969068f0e8ac4eae3b0aa27a
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-06-06 12:57:01 +0800

    Multiple updates

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app     = express();
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-    res.send('Hello World');
+var express = require("express");
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
-app.listen(3000, function() {
-    console.log('Running on port: 3000');
+
+app.listen(3000, function () {
+  console.log("Running on http://localhost:3000/");
 });
--- public/index.js
+++ public/index.js
@@ -74,14 +74,7 @@ function App(){
     </>);
 }
 
-/*ReactDOM.render(
+ReactDOM.render(
   <App/>,
   document.getElementById('root')
-);*/
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
 );
