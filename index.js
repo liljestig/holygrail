6118bcf75452f4c7adf13cbca170378c92cb6021
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-06-06 13:37:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
 var express = require('express');
 var app = express();
+var redis = require("redis");
+var client = redis.createClient();
+
 app.use(express.static('public'));
+
+// init values
+client.mSet("header", 0, "left", 0, "article", 0, "right", 0, "footer", 0);
+client.mGet(
+  ["header", "left", "article", "right", "footer"],
+  function (err, value) {
+    console.log(value);
+  }
+);
+
+function data() {
+  return new Promise((resolve, reject) => {
+    client.mGet(
+      ["header", "left", "article", "right", "footer"],
+      function (err, value) {
+        const data = {
+          header: Number(value[0]),
+          left: Number(value[1]),
+          article: Number(value[2]),
+          right: Number(value[3]),
+          footer: Number(value[4]),
+        };
+        err ? reject(null) : resolve(data);
+      }
+    );
+  });
+}
+
+// get key data
+app.get("/data", function (req, res) {
+  data().then((data) => {
+    console.log(data);
+    res.send(data);
+  });
+});
+
+// plus
+app.get("/update/:key/:value", function (req, res) {
+  const key = req.params.key;
+  let value = Number(req.params.value);
+  client.get(key, function (err, reply) {
+    // new value
+    value = Number(reply) + value;
+    client.set(key, value);
+
+    // return data to client
+    data().then((data) => {
+      console.log(data);
+      res.send(data);
+    });
+  });
+});
+
+/*
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
+*/
+
 app.listen(3000, function() {
     console.log('Running on port 3000');
 });
